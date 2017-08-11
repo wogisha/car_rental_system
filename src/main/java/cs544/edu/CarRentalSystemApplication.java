456094828170d27229ac9d9d86342c09d7933e77
wogisha Benjamin <wogisha@gmail.com>
2017-08-11 11:40:34 -0500

    created the base project

---
+++ src/main/java/cs544/edu/CarRentalSystemApplication.java
@@ -0,0 +1,12 @@
+package cs544.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/cs544/edu/CarRentalSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package cs544.edu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarRentalSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
