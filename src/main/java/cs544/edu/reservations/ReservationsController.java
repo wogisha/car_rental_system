7c6033bd8f685ee9b1fd7b20440676cfb6cd0503
wogisha Benjamin <wogisha@gmail.com>
2017-08-15 11:33:27 -0500

    added access control to reservations section

--- src/main/java/cs544/edu/reservations/ReservationsController.java
+++ src/main/java/cs544/edu/reservations/ReservationsController.java
@@ -1,8 +1,10 @@
 package cs544.edu.reservations;
 
 
+import cs544.edu.entities.Customer;
 import cs544.edu.entities.Reservation;
 import cs544.edu.entities.Vehicle;
+import cs544.edu.userMgmt.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/reservations")
@@ -21,14 +25,27 @@ public class ReservationsController {
     @Autowired
     private ReservationService reservationService;
 
+    @Autowired
+    private CustomerService customerService;
+
     @GetMapping("")
-    public String getReservations(Model model, @RequestParam(value = "q", required = false) String q, @RequestParam(value = "page", defaultValue = "1") String page) {
+    public String getReservations(Model model, @RequestParam(value = "q", required = false) String q, @RequestParam(value = "page", defaultValue = "1") String page, HttpServletRequest httpServletRequest,Principal principal) {
         Page<Reservation> reservationList = null;
 
-        if (q != null && !q.isEmpty()) {
-            reservationList = reservationService.getCustomerReservationsById(q.trim(), Integer.parseInt(page) - 1);
+        if (httpServletRequest.isUserInRole("ROLE_CUSTOMER")) {
+            Customer customer = customerService.findByUsername(principal.getName());
+            if (customer != null) {
+                reservationList = reservationService.getCustomerReservations(customer.getId(), Integer.parseInt(page) - 1);
+            }
+
         } else {
-            reservationList = reservationService.getAllReservations(Integer.parseInt(page) - 1);
+
+
+            if (q != null && !q.isEmpty()) {
+                reservationList = reservationService.getCustomerReservationsById(q.trim(), Integer.parseInt(page) - 1);
+            } else {
+                reservationList = reservationService.getAllReservations(Integer.parseInt(page) - 1);
+            }
         }
 
         model.addAttribute("reservations", reservationList);
@@ -39,7 +56,7 @@ public class ReservationsController {
 
     @GetMapping("add")
     public String makeReservation(Model model, @RequestParam(value = "vehicleId", required = false) Long vehicleId,
-                                  @ModelAttribute Reservation reservation) {
+                                  @ModelAttribute Reservation reservation, HttpServletRequest httpServletRequest, Principal principal) {
         Vehicle vehicleToReserve = null;
 
         try {
@@ -50,16 +67,24 @@ public class ReservationsController {
         }
 
         model.addAttribute("vehicle", vehicleToReserve);
-        model.addAttribute("customers", reservationService.getCustomers());
+
+        if (httpServletRequest.isUserInRole("ROLE_CUSTOMER")) {
+            System.out.println("this is a customer");
+            model.addAttribute("customer", customerService.findByUsername(principal.getName()));
+        } else {
+            model.addAttribute("customers", reservationService.getCustomers());
+        }
+
+
         return "reservations/add";
     }
 
     @PostMapping("add")
     public String saveReservation(Model model, @RequestParam(value = "vehicleId", required = false) Long vehicleId,
                                   RedirectAttributes redirectAttributes,
-                                   @Validated({Reservation.NewReservation.class}) Reservation reservation,
+                                  @Validated({Reservation.NewReservation.class}) Reservation reservation,
 
-                                  BindingResult bindingResult, @RequestParam(value = "customerId") Long customerId) {
+                                  BindingResult bindingResult, @RequestParam(value = "customerId") Long customerId, HttpServletRequest httpServletRequest, Principal principal) {
 
         Vehicle vehicleToReserve = null;
 
@@ -70,10 +95,25 @@ public class ReservationsController {
             return "redirect:/vehicles";
         }
 
+        if (httpServletRequest.isUserInRole("ROLE_CUSTOMER")) {
+
+            Customer customer = customerService.findByUsername(principal.getName());
+            if (customer == null) {
+                redirectAttributes.addFlashAttribute("customerProfileMessage", "Please enter your profile details before you can reserve a car");
+                return "redirect:/cust/profile";
+            }
+
+            model.addAttribute("customer", customer);
+        } else {
+            model.addAttribute("customers", reservationService.getCustomers());
+        }
+
         if (bindingResult.hasErrors()) {
 
             model.addAttribute("vehicle", vehicleToReserve);
-            model.addAttribute("customers", reservationService.getCustomers());
+            model.addAttribute("customerId", customerId);
+
+
             return "reservations/add";
         }
 
