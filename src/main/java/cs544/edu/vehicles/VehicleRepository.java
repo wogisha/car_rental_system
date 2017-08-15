cf050c9138dfd9c9d2cc2c6f9bf41c9bcb441624
wogisha Benjamin <wogisha@gmail.com>
2017-08-14 22:23:31 -0500

    fixed bug

--- src/main/java/cs544/edu/vehicles/VehicleRepository.java
+++ src/main/java/cs544/edu/vehicles/VehicleRepository.java
@@ -2,12 +2,13 @@ package cs544.edu.vehicles;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import cs544.edu.entities.Vehicle;
 import cs544.edu.entities.enums.FuelType;
 import cs544.edu.entities.enums.VehicleStatus;
 
-public interface VehicleRepository extends CrudRepository<Vehicle, Long>{
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
 	public List<Vehicle> findByStatus(VehicleStatus available); 
 	public List<Vehicle> findByFuelType(FuelType type); 
